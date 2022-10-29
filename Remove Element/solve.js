eb09cc33fd591a756bf0520c7ee203bc562739bd
Imranhossain7 <imran.ice.nstu12@gmail.com>
2022-10-29 12:37:45 +0600

    Solved 27 with pointers

--- Remove Element/solve.js
+++ Remove Element/solve.js
@@ -15,6 +15,29 @@
    return pos;
 };
 
+//With pointers
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    var start =0;
+     var end = nums.length-1;
+     
+     while (left <= right){
+         if(nums[start] === val) {
+             nums[start] = nums [end];
+             end-- ;
+         }
+         else{
+             start++;
+         }
+     }
+      return start;
+ }
+
 
 
 	
