65d11a9e70e0be068387c478657fdfb90f6d7fd4
Imranhossain7 <imran.ice.nstu12@gmail.com>
2023-01-24 21:32:08 +0600

    1431. solved using Map

--- 1431. Kids With the Greatest Number of Candies/index.js
+++ 1431. Kids With the Greatest Number of Candies/index.js
@@ -8,14 +8,9 @@ var kidsWithCandies = function (candies, extraCandies) {
   let ret = [];
 
   let max = Math.max(...candies);
-
-  for (let j = 0; j < l; j++) {
-    if (candies[j] + extraCandies >= max) {
-      ret[j] = true;
-    } else {
-      ret[j] = false;
-    }
-  }
+  candies.map((item) =>
+    item + extraCandies >= max ? ret.push(true) : ret.push(false)
+  );
 
   return ret;
 };
