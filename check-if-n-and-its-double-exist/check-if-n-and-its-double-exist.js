0d0bd5f2d59e0b6559c8e91dec645a3ee02e016e
Imran Hossain Shakil <61075990+Imranhossain7@users.noreply.github.com>
2023-03-06 12:22:10 +0600

    Time: 73 ms, Memory: 44.4 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -6,8 +6,7 @@ var checkIfExist = function(arr) {
     arr.sort((a,b) => (a-b));
     
     for (let i=0; i< arr.length;i++){
-        let low = 0 ;
-        let high = arr.length - 1;
+        let low = 0 ,high = arr.length - 1;
         
         while( low <= high) {
             let mid = Math.floor((low+high)/2);
