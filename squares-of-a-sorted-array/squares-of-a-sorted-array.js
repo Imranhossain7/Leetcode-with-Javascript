08480ce946d2ed87567114ebb575a95c4f8a917e
Imran Hossain Shakil <61075990+Imranhossain7@users.noreply.github.com>
2023-03-14 23:11:02 +0600

    Time: 98 ms, Memory: 48.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -1,34 +1,20 @@
-/**
-Using map function
- * @param {number[]} nums
- * @return {number[]}
- */
 var sortedSquares = function(nums) {
+    const n = nums.length;
+    const result = new Array(n);
+    let left = 0;
+    let right = n - 1;
+    let i = n - 1;
     
-    let arr = [];
-    
-    nums.map(myFunction);
-    
-    function myFunction(value){
-        arr.push(value*value);
+    while (left <= right) {
+        if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+            result[i] = nums[left] ** 2;
+            left++;
+        } else {
+            result[i] = nums[right] ** 2;
+            right--;
+        }
+        i--;
     }
     
-   let nums2= arr.sort(function(a,b){return(a-b)});
-    return nums2;
+    return result;
 };
-
-
- /**
- Using array and for loop
- 
- * @param {number[]} nums
- * @return {number[]}
-var sortedSquares = function(nums) {
-    var len = nums.length;
-    let arr = [];
-    for(let i=0;i<len;i++){
-        arr.push(nums[i]*nums[i]);
-    }
-    return arr.sort(function(a,b){return(a-b)});
-};
-*/
