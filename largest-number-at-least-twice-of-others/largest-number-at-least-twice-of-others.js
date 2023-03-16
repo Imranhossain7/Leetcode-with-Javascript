7988a626a6bf90590648158fdff0d575ebb291e8
Imran Hossain Shakil <61075990+Imranhossain7@users.noreply.github.com>
2023-03-16 23:28:50 +0600

    Time: 54 ms, Memory: 42.3 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -3,13 +3,21 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    let sortedNums = [...nums].sort((a,b) => b-a);
+     
+    let max = 0 ;
+    let maxIndex = 0;
+    for(let i=0; i< nums.length;i++){
+        if(nums[i] > max){
+            max = nums[i];
+            maxIndex = i
+        }
+    }
     
     for(let i=0; i<nums.length;i++){
-        if(nums[i] !== sortedNums[0] &&  nums[i]*2 > sortedNums[0]) {
-     return -1;   
+        if(i!== maxIndex &&  nums[i]*2 > max){
+         return -1;   
     }
     }
-    return  nums.indexOf(sortedNums[0]); 
+    return maxIndex ; 
     
 };
